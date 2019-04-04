f745bbc3429eae13913fd9b1a60adc90676994de
leeoandradee <https://github.com/leeoandradee/amigos-das-criancas-sp.git>
2019-04-03 23:36:08 -0300

    Adicionando novas funcionalidade

--- app/src/main/java/breath/mackenzie/com/br/tcc_breath/MainActivity.java
+++ app/src/main/java/breath/mackenzie/com/br/tcc_breath/MainActivity.java
@@ -1,13 +1,47 @@
 package breath.mackenzie.com.br.tcc_breath;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton botaoFoto;
+    private ImageView fotoTirada;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoFoto = (ImageButton) findViewById(R.id.botaoFoto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras  = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            fotoTirada.setImageBitmap(imageBitmap);
+        }
     }
 }
